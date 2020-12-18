33b44ff546c4026b7c406892fdfe568e37a6c97e
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-18 21:27:09 +0400

    getting image with UIImagePickerController is complete

---
+++ project12c/Photo.swift
@@ -0,0 +1,19 @@
+//
+//  Photo.swift
+//  project12c
+//
+//  Created by Tigran on 12/18/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class Photo: NSObject {
+    var name: String
+    var image: String
+    
+    init(name:String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
--- project12c/ViewController.swift
+++ project12c/ViewController.swift
@@ -8,11 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var photos = [Photo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: photos[indexPath.row].image)
+        cell.textLabel?.text = photos[indexPath.row].name
+        return cell
+    }
+    
+    @objc func addTapped() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        let photo = Photo(name: "unknown", image: imageName)
+        photos.append(photo)
+        tableView.reloadData()
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 
 
